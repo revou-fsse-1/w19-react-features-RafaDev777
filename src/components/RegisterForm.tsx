8cdfa7cb5af668354a79a9aebe4adfc411f6d277
Papapanda-PC <rafaeljonathan.work@gmail.com>
2023-06-17 00:32:08 +0700

    clean up

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,4 +1,4 @@
-import { Controller, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
