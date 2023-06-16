895c944374cae8af787dd19e9785164b8e68fe38
Papapanda-PC <rafaeljonathan.work@gmail.com>
2023-06-16 19:56:05 +0700

    register form name validation

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,17 +1,53 @@
+import { Controller, useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+interface IFormData {
+	name: string;
+	email: string;
+	password: string;
+}
+const schema = yup.object({
+	name: yup.string().required(),
+	email: yup.string().email().required(),
+	password: yup.string().required(),
+});
 
 const RegisterForm = () => {
+	// # Validation Function --------------------------
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({ resolver: yupResolver(schema) });
+
+	// # Services?
+	const handleRegister = () => {
+		return null;
+	};
+
 	return (
-		<form className="flex-col items-center flex text-center">
+		<form
+			onSubmit={handleSubmit(handleRegister)}
+			className="flex-col items-center flex text-center"
+		>
+			<input
+				id="name"
+				{...register('name')}
+				className="input-style mt-10"
+				placeholder="Enter your username"
+			></input>
+			{errors?.name && (
+				<span className="mt-[0.25rem] text-sm text-red-600">
+					{errors.name.message}
+				</span>
+			)}
+
 			<input
 				id="email"
-				className="input-style mt-10"
-				placeholder="Please enter your email"
-			></input>
-			<input
-				id="username"
 				className="input-style mt-4"
-				placeholder="Enter your username"
+				placeholder="Please enter your email"
 			></input>
 			<input
 				id="password"
@@ -19,7 +55,9 @@ const RegisterForm = () => {
 				className="input-style mt-4"
 				placeholder="Enter your password"
 			></input>
-			<button className="primary-button mt-10">Register</button>
+			<button type="submit" className="primary-button mt-10">
+				Register
+			</button>
 			<p className="mt-5 text-xs italic text-slate-500">
 				Already have an account?{' '}
 				<Link to="/login" className="text-sky-600">
