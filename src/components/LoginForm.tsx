c10701c3dd34190e306069e17bc19d1ce059e8b3
Papapanda-PC <rafaeljonathan.work@gmail.com>
2023-06-17 01:27:00 +0700

    login validation and auth

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
+import * as yup from 'yup';
+import axios from 'axios';
+import { useState } from 'react';
+interface IFormData {
+	email: string;
+	password: string;
+}
+
+const schema = yup.object({
+	email: yup
+		.string()
+		.email('Please enter a valid email address')
+		.required('Please fill in your emal address'),
+	password: yup
+		.string()
+		.min(5, 'Your password must have minimum 5 digit')
+		.matches(
+			/^(?=.*[0-9])(?=.*[a-zA-Z])/,
+			'Your password must containt at least 1 number and 1 letter'
+		)
+		.required('Please fill in your password'),
+});
 
 const LoginForm = () => {
 	// # Validation Function --------------------------
@@ -8,39 +30,66 @@ const LoginForm = () => {
 		register,
 		handleSubmit,
 		formState: { errors },
-	} = useForm();
-
-	const loginOpt = {
-		username: { required: 'Please fill in your username' },
-		passsword: { required: 'Please fill input your password' },
-	};
+	} = useForm({ resolver: yupResolver(schema), mode: 'onTouched' });
 
 	// # Form Business Function -----------------------
+	const [isError, setIsError] = useState(false);
+	const navigate = useNavigate();
+	const handleLogin = async (input: IFormData) => {
+		try {
+			const { data } = await axios.post(
+				'https://mock-api.arikmpt.com/api/user/login',
+				{
+					email: input.email,
+					password: input.password,
+				}
+			);
+
+			localStorage.setItem('token', data.data.token);
+
+			navigate('/dashboard');
+		} catch (error) {
+			setIsError(true);
+		}
+	};
 
 	// # Test Purpose Function------------------------
-	const navigate = useNavigate();
-	const routeChange = () => {
-		const path = '/dashboard/1';
-		navigate(path);
-	};
 
 	return (
 		<>
-			<form className="flex-col items-center flex opacity-100 transition-opacity ease-in text-center">
+			<form
+				onSubmit={handleSubmit(handleLogin)}
+				className="flex-col items-center flex opacity-100 transition-opacity ease-in text-center"
+			>
 				<input
-					id="username"
+					id="email"
+					{...register('email')}
 					className="input-style mt-5"
 					placeholder="Please enter your username"
 				></input>
+				{errors?.email && (
+					<span className="input-error-message">{errors.email.message}</span>
+				)}
 				<input
 					id="password"
+					{...register('password')}
 					type="password"
 					className="input-style mt-4"
-					placeholder="Enter your password"
+					placeholder="Please enter your password"
 				></input>
-				<button className="primary-button mt-10" onClick={routeChange}>
-					Login
-				</button>
+				{errors?.password && (
+					<span className="input-error-message">{errors.password.message}</span>
+				)}
+				<div className="flex flex-col items-center mt-10">
+					{isError && (
+						<span className="input-error-message mb-2">
+							Please check again your email and password.
+						</span>
+					)}
+					<button type="submit" className="primary-button">
+						Login
+					</button>
+				</div>
 				<p className="mt-2 text-sm italic text-slate-500 transition ease-in-out duration-300">
 					Don't have any account?{' '}
 					<Link to="/register" className="text-sky-600">
